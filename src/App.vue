8d9111042dc350a47ed0449d0179c933abcdfc25
Franz Amersian <114055827+versacodes@users.noreply.github.com>
2023-07-17 17:05:45 +0800

    Added and styled introduction section

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,26 @@
 <template>
 
   <header class="flex justify-between items-center bg-blue-950 py-4 px-6">
-    <h1 class="text-2xl font-bold text-yellow-100">versacodes</h1>
-    <nav class="w-[10rem] flex items-center justify-between">
+    <h1 class="text-lg font-bold text-yellow-100">versacodes</h1>
+    <nav class="w-[7rem] flex items-center justify-between">
       <a>Home</a>
       <a>Projects</a>
     </nav>
   </header>
+
+  <main>
+    <section class="py-10 px-8 bg-neutral-50">
+      <h1 class="text-xl text-center text-zinc-700 mb-2">
+        Hi, my name is<br>
+        <span class="text-zinc-900 mt-1 inline-block font-bold leading-8 text-3xl">Franz Amersian.</span>
+      </h1>
+      <p class="mt-4 text-center text-zinc-700 mx-auto text-sm w-[12rem]">
+        I'm a
+        <span class="font-bold text-zinc-700">Vue.js Front End Developer</span>
+      </p>
+      <button class="bg-red-500 text-sm text-white font-bold p-3 mt-5 mx-auto block">My Projects</button>
+    </section>
+  </main>
   
 </template>
 
