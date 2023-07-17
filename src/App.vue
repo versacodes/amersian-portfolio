34a86014f66bce601ad9c941f21b0a66ad79d358
Franz Amersian <114055827+versacodes@users.noreply.github.com>
2023-07-17 16:19:32 +0800

    Initialized project Added a header section Added styling for header

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <header class="flex justify-between items-center bg-blue-950 py-4 px-6">
+    <h1 class="text-2xl font-bold text-yellow-100">versacodes</h1>
+    <nav class="w-[10rem] flex items-center justify-between">
+      <a>Home</a>
+      <a>Projects</a>
+    </nav>
+  </header>
+  
+</template>
+
+<style scoped>
+
+</style>
