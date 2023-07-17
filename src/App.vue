ecd6fee8c45b327fd035aee48bbd6123b5f5f18c
Franz Amersian <114055827+versacodes@users.noreply.github.com>
2023-07-17 19:13:45 +0800

    Added a footer section

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,11 @@
     <section class="py-10 px-8 bg-neutral-50">
       <h1 class="text-xl text-center text-zinc-700 mb-2">
         Hi, my name is<br>
-        <span class="text-zinc-900 mt-1 inline-block font-bold leading-8 text-3xl">Franz Amersian.</span>
+        <span class="text-zinc-900 mt-1 inline-block font-bold leading-8 text-3xl">Franz Amersian Tiongson.</span>
       </h1>
       <p class="mt-4 text-center text-zinc-700 mx-auto text-sm w-[12rem]">
         I'm a
-        <span class="font-bold text-zinc-700">Vue.js Front End Developer</span>
+        <span class="underline font-bold text-zinc-700">Vue.js Front End Developer</span>
       </p>
       <button class="bg-red-500 text-sm text-white font-bold p-3 mt-5 mx-auto block">My Projects</button>
     </section>
@@ -56,6 +56,10 @@
                type="submit">
     </form>
   </section>
+
+  <footer class="bg-blue-950 px-10 py-8">
+    <p class="text-neutral-300 text-xs">Created by Franz Amersian Tiongson</p>
+  </footer>
 </template>
 
 <style scoped>
